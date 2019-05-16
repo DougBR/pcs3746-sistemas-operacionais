b0b11fb80a0c08d5caf09fd3dd789ca88f6ad353
Thales César <thalesgiriboni@gmail.com>
2019-05-16 15:04:27 -0300

    WIP tarefa 1

---
+++ 1/initramfs/child.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    while (1) {
+        printf("2\n");
+    }
+    return 0;
+}
--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -1,9 +1,18 @@
-#include "hello_world.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include<stdlib.h>
+#include<unistd.h>
 
 int main()
 {
-	while (1) {
-		hello_world();
+	if (fork() != 0) {
+		/* Parent code */
+		while (1) {
+			printf("1\n");
+		}
+		return 0;
+	} else {
+		/* Child code */
+		execve("./child.o", NULL, NULL);
 	}
-	return 0;
 }
