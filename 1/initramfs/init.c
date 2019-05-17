5382dd0aa0837348e449c65afcced1272a33e8a4
Douglas Luan <douglasctm@gmail.com>
2019-05-17 14:31:59 -0300

    changing execve file to executable and making other minor improvements

--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -2,17 +2,30 @@
 #include <sys/types.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<errno.h>
+
+void panic(const char *msg)
+{
+	fprintf(stderr, "%s: %s (errno = %d)\n", msg, strerror(errno), errno);
+	exit(-1);
+}
 
 int main()
 {
-	if (fork() != 0) {
+pid_t pid = fork();
+
+	if (pid == -1 ) {
+    panic("fork failed");
+   }
+  else if (pid){
 		/* Parent code */
 		while (1) {
 			printf("1\n");
 		}
 		return 0;
+    
 	} else {
 		/* Child code */
-		execve("./child.o", NULL, NULL);
+		execve("child", NULL, NULL);
 	}
 }
