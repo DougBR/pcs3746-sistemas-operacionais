77a1200504d26c34c1b8df59815d602d5648eb6c
Douglas Luan <douglasctm@gmail.com>
2019-06-03 21:48:25 -0300

    adding unistd lib for sleep improving identation

--- initramfs/signal_bypassing.c
+++ initramfs/signal_bypassing.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<signal.h>
 #include<string.h>
+#include<unistd.h>
 
 void bypass_sigint(int sig_no)
 {
@@ -12,10 +13,10 @@ int main()
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = &bypass_sigint;
-    sigaction(SIGFPE, &sa,NULL);
+    sigaction(SIGFPE, &sa, NULL);
     while (1) {
       sleep(1);
-        printf("do nothing \n ");
+      printf("do nothing \n ");
     }
     return 0;
 }
